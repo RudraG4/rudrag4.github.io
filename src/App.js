db4b304ff7b9099b44f398e19e84fe388829f2c8
RudraGowda M Patil <rudragowda53@gmail.com>
2022-09-27 19:39:45 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { SEO } from "components";
+import { Analytics } from "components";
 import RouteProvider from "context/providers/RouteProvider";
 import AppRouter from "routers/AppRouter";
-import Config from "config";
 import "./styles.scss";
 
 export default function App() {
   return (
     <>
-      <SEO {...Config.Metadata} />
+      <Analytics />
       <RouteProvider>
         <AppRouter />
       </RouteProvider>
--- src/components/analytics/Analytics.jsx
+++ src/components/analytics/Analytics.jsx
@@ -1,19 +1,19 @@
-const GAnalytics = ({ analyticsId }) => {
-  /* eslint-disable-next-line */
-  if (process.env.NODE_ENV != "production") return;
+import { createPortal } from "react-dom";
+import Config from "config";
 
+const GAScript = () => {
   return (
     <>
       <script
         async
-        src={`https://www.googletagmanager.com/gtag/js?id=${analyticsId}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${Config.Metadata.googleAnalyticsId}`}
       />
 
       <script id="ga-script">
         {` window.dataLayer = window.dataLayer || [];
            function gtag(){ dataLayer.push(arguments); }
            gtag('js', new Date());
-           gtag('config', '${analyticsId}', {
+           gtag('config', '${Config.Metadata.googleAnalyticsId}', {
               page_path: window.location.pathname
            });
         `}
@@ -22,7 +22,12 @@ const GAnalytics = ({ analyticsId }) => {
   );
 };
 
-export default GAnalytics;
+export default () => {
+  /* eslint-disable-next-line */
+  if (process.env.NODE_ENV != "production") return;
+  const head = document.head || document.querySelector("head");
+  return createPortal(<GAScript />, head);
+};
 
 export const sendEvent = (action, category, label, value) => {
   window.gtag?.("event", action, {
